9432d7b16ec2b8b0d3f56abbd9895a51a82beb9d
YS22 <1774507011@qq.com>
2017-05-14 17:31:02 +0800

    3

--- blog/views.py
+++ blog/views.py
@@ -11,13 +11,13 @@ from django.views.decorators.csrf import requires_csrf_token
 def index(request):
 	user=Employee.objects.all()
 	form = AddForm()
-	#if request.method == 'POST':
-		#form = AddForm(request.POST)
-	if form.is_valid():
-		name= form.cleaned_data['name']
-		age= form.cleaned_data['age']
-		gender= form.cleaned_data['gender']
-		u=Employee(name=name,age=age,gender=gender)
-		u.save()
+	if request.method == 'POST':
+		form = AddForm(request.POST)
+		if form.is_valid():
+			name= form.cleaned_data['name']
+			age= form.cleaned_data['age']
+			gender= form.cleaned_data['gender']
+			u=Employee(name=name,age=age,gender=gender)
+			u.save()
            
 	return render(request, 'index.html', {'form':form, 'user':user})
